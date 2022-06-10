942b1e1e8f0aedf816d13ad03a7651c8aa74a9c6
andreysobol <andreysobol@protonmail.com>
2022-06-10 19:26:21 +0300

    businessLogic++

--- ZkBugBounty.sol
+++ ZkBugBounty.sol
@@ -3,7 +3,10 @@ pragma solidity ^0.8.13;
 
 contract ZkBugBounty {
 
-    function businessLogic() public {
+    function businessLogic(bytes calldata transition, bytes32 nextState /*, proofData */) public {
+        bytes32 transitionHash = sha256(transition);
+        
+        /* verify_proof(proofData, transitionHash, nextState) */
         return;
     }
 
