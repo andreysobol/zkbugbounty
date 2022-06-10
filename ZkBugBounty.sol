1d905f43dd8a2ddce58be3630ce1090317c56e0e
andreysobol <andreysobol@protonmail.com>
2022-06-10 17:48:12 +0300

    Init ZkBugBounty.sol

---
+++ ZkBugBounty.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+contract ZkBugBounty {
+
+    function businessLogic() public {
+        return;
+    }
+
+    function proofOfHack() public {
+        return;
+    }
+
+}
