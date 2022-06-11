b5e13fc6515be7197b9d426def1017b7eb08a6b6
Andrey Sobol <andreysobol@protonmail.com>
2022-06-11 19:08:21 +0200

    Hello world hardhat

---
+++ contracts/contracts/Hello.sol
@@ -0,0 +1,8 @@
+
+// SPDX-License-Identifier: MIT
+// compiler version must be greater than or equal to 0.8.13 and less than 0.9.0
+pragma solidity ^0.8.0;
+
+contract HelloWorld {
+    string public greet = "Hello World!";
+}
